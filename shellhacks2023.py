d2f9a84b265725d7a9cda34ddfe06f443c7477a4
Mohamadm606 <125500847+Mohamadm606@users.noreply.github.com>
2023-09-16 13:34:04 -0400

    Update shellhacks2023.py
    esg function

--- shellhacks2023.py
+++ shellhacks2023.py
@@ -1,7 +1,9 @@
 import requests
-import json 
 
-def print_esg_info(ticker):
+key = "8c0b623ea5msh98642c408604475p1c7ea9jsn360b852a561d"
+
+#get python dictionary data
+def get_esg_info(ticker):
     url = "https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/get-esg-scores"
 
     querystring = {"symbol":ticker,"region":"US","lang":"en-US"}
@@ -11,24 +13,9 @@ def print_esg_info(ticker):
         "X-RapidAPI-Host": "apidojo-yahoo-finance-v1.p.rapidapi.com"
     }
 
-    response = requests.get(url, headers=headers, params=querystring)
+    data = requests.get(url, headers=headers, params=querystring).json()
+    newData = data["quoteSummary"]["result"][0]["esgScores"]
 
-    print(json.dumps(response.json(), sort_keys=True, indent=4))
-
-
-def get_ticker(str):
-
-    url = "https://apidojo-yahoo-finance-v1.p.rapidapi.com/auto-complete"
-
-    querystring = {"q":str,"region":"US"}
-
-    headers = {
-        "X-RapidAPI-Key": "8c0b623ea5msh98642c408604475p1c7ea9jsn360b852a561d",
-        "X-RapidAPI-Host": "apidojo-yahoo-finance-v1.p.rapidapi.com"
-    }
-
-    response = requests.get(url, headers=headers, params=querystring)
+    keys = ["environmentScore", "governanceScore", "socialScore"]
     
-    print(json.dumps(response.json(), sort_keys=True, indent=4))
-
-get_ticker("tesla")
+    return(getValues(newData, keys))
