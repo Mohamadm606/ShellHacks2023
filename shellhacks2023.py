ebb31f94d60160a4ce04b2c231f54c3b524fbf2b
Daniel E. Gonzalez <125385878+danielg-onzalez@users.noreply.github.com>
2023-09-17 10:58:46 -0400

    Update shellhacks2023.py

--- shellhacks2023.py
+++ shellhacks2023.py
@@ -1,7 +1,14 @@
+import requests
+import math
+
+key = "8c0b623ea5msh98642c408604475p1c7ea9jsn360b852a561d"
+
+
+# get python dictionary data
 def get_esg_info(ticker):
     url = "https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/get-esg-scores"
 
-    querystring = {"symbol":ticker,"region":"US","lang":"en-US"}
+    querystring = {"symbol": ticker, "region": "US", "lang": "en-US"}
 
     headers = {
         "X-RapidAPI-Key": "8c0b623ea5msh98642c408604475p1c7ea9jsn360b852a561d",
@@ -9,14 +16,13 @@ def get_esg_info(ticker):
     }
 
     data = requests.get(url, headers=headers, params=querystring).json()
-    
     newData = data["quoteSummary"]["result"][0]["esgScores"]
-    
-    return(get_values(newData))
+
+    return (get_values(newData))
 
 
 def get_values(json_dict):
-    keys = ["environmentScore", "governanceScore", "socialScore","totalEsg"]
+    keys = ["environmentScore", "governanceScore", "socialScore", "totalEsg"]
 
     numeric_values = []
 
@@ -26,3 +32,4 @@ def get_values(json_dict):
     numeric_values.append(math.ceil(float(json_dict["totalEsg"]["fmt"])))
 
     return numeric_values
+
