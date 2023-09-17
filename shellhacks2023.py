e00407b2dbfb2f4c763dc4caab841ef3b7245ef7
Mohamadm606 <125500847+Mohamadm606@users.noreply.github.com>
2023-09-17 01:08:42 -0400

    Update shellhacks2023.py

--- shellhacks2023.py
+++ shellhacks2023.py
@@ -1,9 +1,3 @@
-import requests
-import math
-
-key = "8c0b623ea5msh98642c408604475p1c7ea9jsn360b852a561d"
-
-#get python dictionary data
 def get_esg_info(ticker):
     url = "https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/get-esg-scores"
 
@@ -15,6 +9,7 @@ def get_esg_info(ticker):
     }
 
     data = requests.get(url, headers=headers, params=querystring).json()
+    
     newData = data["quoteSummary"]["result"][0]["esgScores"]
     
     return(get_values(newData))
@@ -31,5 +26,3 @@ def get_values(json_dict):
     numeric_values.append(math.ceil(float(json_dict["totalEsg"]["fmt"])))
 
     return numeric_values
-
-
